45b356ef43e0a42ef83c87a636c13440163b9784
Srihari Jala <srihari_j@yahoo.com>
2024-10-15 17:55:41 -0400

    k8 file modified

--- iac/ecs_fargate/main.tf
+++ iac/ecs_fargate/main.tf
@@ -343,10 +343,7 @@ resource "aws_instance" "bastion" {
   # Code for installing the softwares!
   provisioner "remote-exec" {
     inline = [
-      "sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm",
-      "sudo dnf install mysql80-community-release-el9-1.noarch.rpm -y",
-      "sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023",
-      "sudo dnf install mysql-community-client -y"
+      "sudo um install mysql"
     ]
   }
   
