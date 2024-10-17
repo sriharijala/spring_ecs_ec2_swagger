4fbad5146fcb1f55d9ceb5bb3a34be9bb97ed964
Srihari Jala <srihari_j@yahoo.com>
2024-10-17 18:23:50 -0400

    Updated Architecture Drawing

--- iac/ecs_ec2/outputs.tf
+++ iac/ecs_ec2/outputs.tf
@@ -1,7 +1,7 @@
 
 output "alb_url" {
-  value = format("%s%s%s", "http://", aws_lb.main.dns_name,":8080/swagger-ui/index.html")
-  
+  value = format("%s%s%s", "http://", aws_lb.main.dns_name, ":8080/swagger-ui/index.html")
+
 }
 
 output "mysql_db_url" {
@@ -11,3 +11,12 @@ output "mysql_db_url" {
 output "mysql_host" {
   value = aws_db_instance.mysql.address
 }
+
+output "public_cidr" {
+  value = aws_subnet.public[*].cidr_block
+}
+
+output "private_cidr" {
+  value = aws_subnet.private[*].cidr_block
+}
+
