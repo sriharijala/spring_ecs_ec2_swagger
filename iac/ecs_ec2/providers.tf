4b39f3a3430b9b55e0bb9fcc83610f09cc9e82ee
Srihari Jala <srihari_j@yahoo.com>
2024-10-17 19:01:29 -0400

    updated the Terraform providers

--- iac/ecs_ec2/providers.tf
+++ iac/ecs_ec2/providers.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
 
 provider "aws" {
   profile = "default"
