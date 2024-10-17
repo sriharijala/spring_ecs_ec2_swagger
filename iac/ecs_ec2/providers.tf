e73aaf531cf2a4d51e121333c78910d6ac05c893
Srihari Jala <srihari_j@yahoo.com>
2024-10-17 19:17:07 -0400

    final push

--- iac/ecs_ec2/providers.tf
+++ iac/ecs_ec2/providers.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
-
 provider "aws" {
   profile = "default"
   region  = var.AWS_REGION
